0588c2bcddd5e59f4b0f06c7e7534f0184cc12f6
MrAndreID <andreaadam215@gmail.com>
2021-01-27 19:39:31 +0700

    add func. for log;

---
+++ gomiddleware.go
@@ -0,0 +1,18 @@
+package gomiddleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func Log(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		log.Println("Accepted")
+		log.Println(request.Method + " " + request.Host + request.URL.String())
+
+		next(response, request)
+
+		log.Println("Closing")
+		log.Println()
+	})
+}
