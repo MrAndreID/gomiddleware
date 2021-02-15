1f128ef6bc2d38dd7e57451c0a47db5c511d2c6c
MrAndreID <andreaadam215@gmail.com>
2021-02-16 02:26:25 +0700

    improve in log func.;

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/MrAndreID/gomiddleware
 
 go 1.15
 
-require github.com/MrAndreID/gohelpers v1.2.0
+require (
+	github.com/MrAndreID/gohelpers v1.4.1
+	github.com/MrAndreID/golog v1.1.3
+)
--- gomiddleware.go
+++ gomiddleware.go
@@ -1,22 +1,20 @@
 package gomiddleware
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
+	"github.com/MrAndreID/golog"
 	helpers "github.com/MrAndreID/gohelpers"
 )
 
 func Log(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		log.Println("Accepted")
-		log.Println("[" + request.Method + "] " + request.Host + request.URL.String())
+		golog.Info("Accepted")
+		golog.Info("[" + request.Method + "] " + request.Host + request.URL.String())
 
 		next(response, request)
 
-		log.Println("Closing")
-		fmt.Println()
+		golog.Info("Closing")
 	})
 }
 
